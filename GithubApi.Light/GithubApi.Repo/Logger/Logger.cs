f21c59fa2e3cb3968a82c9cd46cc54e50db92be7
MainSkown <83239861+MainSkown@users.noreply.github.com>
2021-05-13 20:05:40 +0200

    Updated Version of GithubApi
    Added: 
    1. Onion Architecture
    2. Getting path to log folder from appsettings.json
    3. Getting Uri from appsettings.json
    4. Changed connection to github from RestEase to HttpClient

---
+++ GithubApi.Light/GithubApi.Repo/Logger/Logger.cs
@@ -0,0 +1,22 @@
+﻿using GithubApi.Data;
+using Microsoft.Extensions.Options;
+using Serilog;
+
+namespace GithubApi.Repo.Logger
+{
+    public class Logger : ILogger
+    {              
+        private readonly string _logPath;
+
+        public Logger(IOptions<LinkOptions> options)
+        {
+            _logPath = options.Value.LogPath;
+        }
+
+        public void LoggError(string str)
+        {
+            var log = new LoggerConfiguration().WriteTo.File(_logPath, rollingInterval: RollingInterval.Day).CreateLogger();
+            log.Information(str);                        
+        }
+    }
+}
---
+++ GithubApi.Light/GithubApi/Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using GithubApi.Repo.Logger;
+using GithubApi.Repo.Models;
+using GithubApi.Repo;
+using GithubApi.Service.Middleware;
+using GithubApi.Data;
+
+namespace GithubApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase("UserList"));
+
+            services.Configure<LinkOptions>(Configuration.GetSection("LinkOptions"));
+
+            services.AddScoped<IGithubRepo, GithubRepo>();
+            services.AddSingleton<ILogger, Logger>();
+
+            services.AddHttpClient();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCustomMiddleware<ExceptionCatchMiddleware>();
+            
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
