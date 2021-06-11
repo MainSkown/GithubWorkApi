6ed584af3de6a6077282e4f1f814cb386ad74604
MainSkown <83239861+MainSkown@users.noreply.github.com>
2021-06-11 20:32:40 +0200

    Update 1.2.3
    Added:
    1. NotFoundException
    2. ListService and ListRepo

---
+++ GithubApi-1.2.3.Light/GithubApi.Repo/ListRepo.cs
@@ -0,0 +1,55 @@
+﻿using GithubApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubApi.Repo
+{
+    public class ListRepo : IListRepo
+    {
+        private readonly IListService _listService;
+        private readonly IGithubRepo _repository;
+        public ListRepo(IListService listService, IGithubRepo repo)
+        {
+            _listService = listService;
+            _repository = repo;
+        }
+
+        public async Task<User> CreateUser(string name)
+        {
+            var user = await _repository.CreateUser(name);
+
+            if(user == null)
+            {
+                throw new NotFoundException();
+            }
+
+            await _listService.AddUser(user);
+
+            return user;
+        }
+
+        public async Task DeleteUser(int id)
+        {
+            bool succeed = await _listService.DeleteUserFromList(id);
+
+            if (succeed == false)
+            {
+                throw new NotFoundException();
+            }
+        }
+
+        public async Task UpdateUserList(long id, string name)
+        {
+            var user = await _repository.CreateUser(name);
+            var tempUser = await _listService.UpdateUser(id, user);
+            
+            if(tempUser == null)
+            {
+                throw new NotFoundException();
+            }
+        }
+    }
+}
---
+++ GithubApi-1.2.3.Light/GithubApi.Service/GithubListController.cs
@@ -0,0 +1,72 @@
+﻿using GithubApi.Data;
+using GithubApi.Repo;
+using GithubApi.Repo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GithubApi.Service
+{
+    [Route("api/github/list")]
+    [ApiController]
+    /// Summary: Operates on list for keeping chosen users
+    public class GithubListController : ControllerBase
+    {
+        private readonly IListRepo _repository;
+        private readonly IListService _listService;
+        public GithubListController(IListRepo repository, IListService listService) 
+        {
+            _repository = repository;
+            _listService = listService;
+        }
+           
+        
+        //Get api/github/list
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> ReturnAddedUsers()
+        {
+            var users = await _listService.ReturnAllUsers();
+
+            //Need to state this like this, because it makes error otherwise
+            return users.ToList();
+        }
+        
+        //GET api/github/list/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> ReturnUser(long id)
+        {
+            var user = await _listService.ReturnUserByID(id);
+
+            return user;
+        }
+
+        //POST api/github/list/{name}
+        [HttpPost("{name}")]
+        public async Task<ActionResult<User>> CreateUser(string name)
+        {
+            var user = await _repository.CreateUser(name);
+
+            return user;
+        }
+
+        //PUT api/github/list/{id}/{name}
+        [HttpPut("{id}/{name}")]
+        public async Task<ActionResult> UpdateUserList(long id, string name)
+        {
+            await _repository.UpdateUserList(id, name);
+
+            return NoContent();
+        }
+
+        //DELETE api/github/list/{id}
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            await _repository.DeleteUser(id);      
+           
+            return NoContent();
+        }
+
+    }
+}
